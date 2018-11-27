37b22d89c095e4dc2912d16a5e4777f4c673489f
Janine Luk <janineluk95@gmail.com>
2018-11-27 14:14:14 +0000

    automatically backed up by learn

--- lib/car.rb
+++ lib/car.rb
@@ -3,7 +3,11 @@ require_relative "./vehicle.rb"
 class Car < Vehicle 
   
   def go 
+<<<<<<< HEAD
     "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+=======
+    "VRRR"
+>>>>>>> e0829c6996314beb1732794d27fce431e483de27
   end
   
 end
